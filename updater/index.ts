8193a69a9bb6b83c94a4618a61bee970eae5bc29
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-19 20:09:00 +0000

    updater url added

--- updater/index.ts
+++ updater/index.ts
@@ -8,7 +8,15 @@ const HOSTNAME = '0.0.0.0';
 const app = new Application();
 const router = new Router();
 
-router.get("/", ({ response }) => {
+router.get("/", ({ response,  }) => {
+    response.status = 400;
+    response.body = "Hello World!";
+});
+
+router.get("/v1/:target/:arch/:current_version", ({ response, params }) => {
+    console.log('params: ', params);
+
+    response.status = 400;
     response.body = "Hello World!";
 });
 
