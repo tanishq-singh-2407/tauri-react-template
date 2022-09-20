6c8263867c4f02c688cbd6e302dfb33cc530fb52
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-20 08:47:50 +0000

    v0.0.11

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Counter from "@/components/counter";
+import { useEffect } from "react";
 
 const Home = () => {
+    useEffect(() => alert("Hey!"), []);
+
     return (
         <div className="min-h-visible h-full w-full">
             <main className="h-full w-full flex justify-center items-center">
