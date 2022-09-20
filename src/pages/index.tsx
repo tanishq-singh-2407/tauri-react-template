c2c6ce5131f9f30e7390b4e33b0b3a2f7a08a2c7
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-20 17:33:46 +0000

    workflow discord job added, and build, dev seperated for tauri and web

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
-import Counter from "@/components/counter";
+import { type } from '@tauri-apps/api/os';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+    const TAURI_APP: boolean = import.meta.env.VITE_TAURI_APP as string === "true";
+    const [os, setOs] = useState<string>("");
+
+    useEffect(() => {
+        if (TAURI_APP) {
+            (async () => {
+                const osType = await type();
+                setOs(osType);
+            })();
+        }
+
+        else setOs("Web");
+    }, []);
+
     return (
         <div className="min-h-visible h-full w-full">
             <main className="h-full w-full flex justify-center items-center">
-                <Counter init={2} />
+                <span>{os}</span>
             </main>
         </div>
     );
