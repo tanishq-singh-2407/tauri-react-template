7db192d15d2f34b34fc28ba463d65486bfd11b09
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-20 13:10:56 +0000

    workflow updated

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -7,20 +7,20 @@ interface CounterInterface {
 const Counter = ({ init }: CounterInterface) => {
     const { count, inc, dec } = useCount({ init });
     return (
-        <div className="aspect-video max-w-xs w-full rounded-md shadow-sm flex flex-col justify-evenly items-center border border-slate-200">
+        <div className="aspect-video max-w-xs mx-5 w-full rounded-md shadow-sm flex flex-col justify-evenly items-center border border-slate-200">
             <span className="text-lg font-bold text-slate-900">{count}</span>
             <div className="flex w-full justify-evenly">
-                <button
-                    className="h-10 w-32 border border-slate-200 rounded-md font-bold text-lg duration-300 hover:shadow active:shadow-none"
-                    onClick={inc}
-                >
-                    +
-                </button>
                 <button
                     className="h-10 w-32 border border-slate-200 rounded-md font-bold text-lg duration-300 hover:shadow active:shadow-none"
                     onClick={dec}
                 >
                     -
+                </button>
+                <button
+                    className="h-10 w-32 border border-slate-200 rounded-md font-bold text-lg duration-300 hover:shadow active:shadow-none"
+                    onClick={inc}
+                >
+                    +
                 </button>
             </div>
         </div>
