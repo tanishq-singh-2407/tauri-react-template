fa7a23d3d394704783c969f267eefb3e3f9ff27e
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-20 14:44:42 +0000

    Made repo changes - v1.0.17

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
 import Counter from "@/components/counter";
-import { useEffect } from "react";
 
 const Home = () => {
-    useEffect(() => {
-        setTimeout(() => alert("Hey! Hi"), 10 * 1000);
-    }, []);
-
     return (
         <div className="min-h-visible h-full w-full">
             <main className="h-full w-full flex justify-center items-center">
