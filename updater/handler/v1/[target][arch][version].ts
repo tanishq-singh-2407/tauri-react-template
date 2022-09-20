2040f7b595a36bca1d243008fcb08fde9b067e4e
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-20 08:15:10 +0000

    updater-5

--- updater/handler/v1/[target][arch][version].ts
+++ updater/handler/v1/[target][arch][version].ts
@@ -20,6 +20,7 @@ const handler: oak.RouterMiddleware<
     Record<string, string>
 > = async ({ response, params, request }) => {
     const { target, arch, current_version } = params;
+    console.log(params);
 
     if (target && arch && current_version) {
         const latestRelease = await getLatestRelease(request);
