446fa255b4717b133629c56d02f069fdc531f5b5
Tanishq Singh <tanisqhsingh640@gmail.com>
2022-09-22 13:36:47 +0530

    Added splash-screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,12 @@
-import { confirm } from '@tauri-apps/api/dialog';
-import { useState } from 'react';
-
 const IS_APP: boolean = import.meta.env.VITE_IS_APP as string === "true";
 
 const Home = () => {
-    const [is, setIs] = useState<boolean>(false);
-
     return (
         <div className="min-h-visible h-full w-full">
             <main className="h-full w-full flex justify-center items-center">
-                {
-                    IS_APP &&
-                    <span
-                        className='text-black font-semibold text-base cursor-pointer'
-                        onClick={async() => {
-                            const so = await confirm('Just a dialog box api test. Are you sure?', { title: 'Tanishq Singh', type: 'warning' });
-                            setIs(so);
-                        }}
-                        style={{
-                            color: is ? "red" : "black"
-                        }}
-                    >
-                        Confirm
-                    </span>
-                }
+                
+                <span>hello</span>
+                
             </main>
         </div>
     );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@
 module.exports = {
 	content: [
 		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
+		"./src/**/*.{js,ts,jsx,tsx,html}",
 	],
 	theme: {
 		extend: {
